461a7537b0dc66310cdb72f76d0ece918cb4707b
nahumsa <nahumsaa@gmail.com>
2025-09-01 18:36:25 -0300

    chore: fix settingsconfigdict

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,9 @@
-from pydantic import ConfigDict, computed_field
-from pydantic_settings import BaseSettings
+from pydantic import computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = ConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     db_user: str
     db_password: str
     db_host: str
